888cbff970621cef8aaa93411a1d08956c6d78b5
jdboisvert <info.jeffreyboisvert@gmail.com>
2023-04-07 21:43:19 -0400

    Began basic structure of the project.

--- checkmigrations/src/main.rs
+++ checkmigrations/src/main.rs
@@ -8,9 +8,26 @@ struct Cli {
     path_to_app: std::path::PathBuf,
 }
 
+const DJANGO: &str = "django";
+
 
 fn main() {
     let args = Cli::parse();
     println!("framework: {}", args.framework);
     println!("path_to_app: {}", args.path_to_app.display());
+
+    let framework = args.framework; 
+    let path = args.path_to_app;
+
+    match framework.as_str() {
+        DJANGO => {
+            println!("django was selected!");
+            println!("path: {}", path.display());
+            // checkmigrations::django::check_migrations(path);
+        },
+        _ => {
+            println!("unknown framework");
+        }
+    }
+
 }
---
+++ docs/development.md
@@ -0,0 +1,9 @@
+# Getting Started
+
+## Prerequisites
+1. Install Rust and Cargo (https://www.rust-lang.org/tools/install)
+
+## Building
+1. Clone the repository
+2. Run `cargo build` to build the project
+3. Run `cargo run` to run the project (ex: `cargo run -- django ./path/to/project`)
