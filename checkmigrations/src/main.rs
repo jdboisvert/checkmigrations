df27ee7b0c99f021a362b56d198ef4aa1d4eb9d2
jdboisvert <info.jeffreyboisvert@gmail.com>
2023-04-07 21:36:34 -0400

    Added basic commmand line interface that accepts which webframework to use and where the path of the app lives to check.

--- checkmigrations/src/main.rs
+++ checkmigrations/src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    // The framework to use
+    framework: String,
+    // The path to the migrations directory to check
+    path_to_app: std::path::PathBuf,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("framework: {}", args.framework);
+    println!("path_to_app: {}", args.path_to_app.display());
 }
